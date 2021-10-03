0c77aaaa65f02486351fb583a11fb65de822da21
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2021-10-03 22:40:47 +0300

    imports done

---
+++ python_automation.py
@@ -0,0 +1,9 @@
+# import http requests
+import requests
+# for web scrapping
+from bs4 import BeautifulSoup
+# email body
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# system time and date manipulation
+import datetime
